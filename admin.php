d66edcf0e5b8b6bdfafd61bb19067ff194be500e
Crazyafk <gill.fmk@oundleschool.org.uk>
2021-10-11 10:34:40 +0100

    added admin.php

---
+++ admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Yet another website</title>
+</head>
+<body>
+
+<p>Hello Admin</p>
+
+<a href="users.php">Add Users</a><br>
+<a href="subjects.php">Add Subjects</a><br>
+<a href="pupildoessubject.php">Signup Pupils to Subjects</a><br>
+<br><br>
+<a href=http://localhost/phpmyadmin/db_structure.php?server=1&db=options>Manage Database</a>
+
+</body>
+</html>
