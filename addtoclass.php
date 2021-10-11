56c7f075d6a11db9e4c38a99e5ac051d2aba1fc3
Crazyafk <gill.fmk@oundleschool.org.uk>
2021-10-11 10:28:34 +0100

    added pupildoessubject support

---
+++ addtoclass.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Yet another website</title>
+</head>
+<body>
+
+<?php
+    header('Location:pupildoessubject.php');
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
+    error_reporting(E_ALL);
+    try{
+        include_once('connection.php');
+        array_map("htmlspecialchars",$_POST);
+
+        $stmt = $conn->prepare("INSERT INTO TblPupilStudies(SubjectID,UserID,ClassPosition,ClassGrade,ExamMark,Comment)VALUES(:SubjectID,:UserID, NULL, NULL, NULL, NULL)");
+
+        $stmt->bindParam(":SubjectID",$_POST["subject"]);
+        $stmt->bindParam(":UserID",$_POST["student"]);
+
+        $stmt->execute();
+    }
+    catch(PDOException $e)
+    {
+        echo "error".$e->getMessage();
+    }
+    $conn=null;
+?>
+
+</body>
+</html>
---
+++ pupildoessubject.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Yet another website</title>
+</head>
+<body>
+
+<form action="addtoclass.php" method="post">
+    <select name = "student">
+    <?php
+        include_once("connection.php");
+        $stmt = $conn->prepare("SELECT * FROM TblUsers WHERE Role = 0 ORDER BY Surname ASC");
+        $stmt->execute();
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            echo("<option value=".$row["UserID"].">".$row["Surname"].",".$row["Forename"]."</option>");
+        }
+    ?>
+    </select>
+
+    <select name = "subject">
+    <?php
+        include_once("connection.php");
+        $stmt = $conn->prepare("SELECT * FROM TblSubjects ORDER BY Subjectname ASC");
+        $stmt->execute();
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            echo("<option value=".$row["SubjectID"].">".$row["Subjectname"].",".$row["Teacher"]."</option>");
+        }
+    ?>
+    </select>
+
+    <input type="submit" value="Add to Class">
+</form>
+
+</body>
+</html>
