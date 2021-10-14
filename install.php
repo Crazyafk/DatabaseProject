e365cd042f27387d2f56dc547c7e5be329d8c8a7
Crazyafk <gill.fmk@oundleschool.org.uk>
2021-10-14 10:25:04 +0100

    first try

---
+++ install.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Yet another website</title>
+</head>
+<body>
+
+<?php
+include_once("connection.php");
+
+//Create Tables
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUser; CREATE TABLE TblUsers(
+    UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Gender VARCHAR(1) NOT NULL,
+    Surname VARCHAR(20) NOT NULL,
+    Forename VARCHAR(20) NOT NULL,
+    Password VARCHAR(20) NOT NULL,
+    House VARCHAR(20) NOT NULL,
+    Year INT(2) NOT NULL,
+    Role TINYINT(1)
+    )");
+$stmt->execute();
+$stmt->closeCursor();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblSubjects; CREATE TABLE TblSubjects(
+    SubjectID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Subjectname VARCHAR(20) NOT NULL,
+    Teacher VARCHAR(20) NOT NULL,
+    )");
+$stmt->execute();
+$stmt->closeCursor();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblPupilStudies; CREATE TABLE TblPupilStudies(
+    SubjectID INT(4) UNSIGNED,
+    UserID INT(4) UNSIGNED,
+    Classposition INT(2),
+    Classgrade VARCHAR(2),
+    Exammark INT(2),
+    Comment VARCHAR(500) NOT NULL,
+    )");
+$stmt->execute();
+$stmt->closeCursor();
+
+//Create Test Data
+function newUser($gender,$surname,$forename,$password,$house,$year,$role)
+{
+    $stmt = $conn->prepare("INSERT INTO TblUsers(UserID,Gender,Surname,Forename,Password,House,Year,Role)
+    VALUES(null,:gender,:surname,:forename,:password,:house,:year,:role)");
+
+    $stmt->bindParam(':forename',$forename);
+    $stmt->bindParam(':surname',$surname);
+    $stmt->bindParam(':house',$house);
+    $stmt->bindParam(':year',$year);
+    $stmt->bindParam(':password',$password);
+    $stmt->bindParam(':gender',$gender);
+    $stmt->bindParam(':role',$role);
+
+    $stmt->execute();
+    $stmt->closeCursor();
+}
+
+newUser("M","Mama","Joe","password","Fuxx",420,2);
+newUser("F","Pupil","Learny","password2","Textbook",4,0);
+newUser("M","Pupil","VOID","3password","DEATH",-1,0);
+newUser("M","Pupil","Dumbass","password2","Textbook",9,0);
+newUser("F","Pupil","Pupil","passw0_rd","Fuxx",13,0);
+newUser("M","Vanker","Boring","physicsisfun","DEATH",4,1);
+newUser("F","Boring","Similarli","englishisfun","Textbook",3,1);
+
+function newSubject($subjectname,$teacher)
+{
+    $stmt = $conn->prepare("INSERT INTO TblSubjects(SubjectID,Subjectname,Teacher)VALUES(NULL,:subjectname,:teacher)");
+
+    $stmt->bindParam(":subjectname",$subjectname);
+    $stmt->bindParam(":teacher",$teacher);
+
+    $stmt->execute();
+    $stmt->closeCursor();
+}
+
+newSubject("Physics","Mr Boring Vanker");
+newSubject("English","Similarli Boring");
+
+function newPupilStudy($subjectid, $userid)
+{
+    $stmt = $conn->prepare("INSERT INTO TblPupilStudies(SubjectID,UserID,ClassPosition,ClassGrade,ExamMark,Comment)VALUES(:SubjectID,:UserID, NULL, NULL, NULL, NULL)");
+
+    $stmt->bindParam(":SubjectID",$subjectid);
+    $stmt->bindParam(":UserID",$userid);
+
+    $stmt->execute();
+    $stmt->closeCursor();
+}
+
+newPupilStudy(2,1);
+newPupilStudy(3,2);
+newPupilStudy(4,1);
+newPupilStudy(5,2);
+?>
+
+</body>
+</html>
