28b071b30d3748d414e5fee6c5cb24e65eef4a60
Crazyafk <61631722+Crazyafk@users.noreply.github.com>
2021-11-02 09:54:43 +0000

    didn't save this one last time :/

--- addtoclass.php
+++ addtoclass.php
@@ -12,6 +12,12 @@
     error_reporting(E_ALL);
     try{
         include_once('connection.php');
+
+        session_start();  
+        if (!isset($_SESSION['name'])) 
+        {    
+            header("Location:login.php"); 
+        }
         array_map("htmlspecialchars",$_POST);
 
         $stmt = $conn->prepare("INSERT INTO TblPupilStudies(SubjectID,UserID,ClassPosition,ClassGrade,ExamMark,Comment)VALUES(:SubjectID,:UserID, NULL, NULL, NULL, NULL)");
